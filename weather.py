330bb09e731276d417b614f57e2c3fa8ffe4b0eb
Ayaan Shaikh <Samx7091@gmail.com>
2024-01-20 13:32:26 +0530

    added formatting

--- weather.py
+++ weather.py
@@ -5,12 +5,13 @@ API_KEY="bce8f81170fb157c484394cdbb54db16"
 
 city = input("Enter city name : ")
 
-# url = "https://api.openweathermap.org/data/2.5/weather?q=London,&APPID=bce8f81170fb157c484394cdbb54db16"
-url = BASE_URL + "q=" + city + ",&APPID="+API_KEY
+
+
+url = f"{BASE_URL}q={city}&APPID={API_KEY}"
 
 
 response = requests.get(url).json()
-# print(response)
+
 
 temp = response['main']['temp']
 temp_min = response['main']['temp_min']
@@ -25,8 +26,8 @@ print(f"Weather in Mumbai: {weather}, {description}")
 
 print(f"The temperature in {city}: {temp} K")
 print(f"Temperature in {city} feels like: {feels_like} k")
-print(f"Minimum temperature: {temp_min}k")
-print(f"Maximum temperature: {temp_max}k")
+print(f"Minimum temperature: {temp_min} k")
+print(f"Maximum temperature: {temp_max} k")
 
 
 print(f"Humidity: {humidity} %")
